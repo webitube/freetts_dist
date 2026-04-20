f0172d8e646a7455a9d0cd01e2a2716864f0d112
Ron Mar <webitube-github@yahoo.com>
2026-04-20 14:59:42 -0700

    * updated vite config for github

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  // This is the important part for GitHub Pages!
-  // It ensures assets are loaded from /repository-name/assets/
-  base: '/freetts_dist/', 
-})
